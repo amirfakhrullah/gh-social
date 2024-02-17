5d7e89ff13ae0c16fdc2910e87a939e26334e9fe
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2024-02-17 17:48:16 +0800

    done

---
+++ src/app/api/trpc/[trpc].ts
@@ -0,0 +1,33 @@
+import { type NextRequest } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { env } from "@/env.mjs";
+import { createContextInner } from "@/server/api/context";
+import { appRouter } from "@/server/api/root";
+
+function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    req,
+    endpoint: "/api/trpc",
+    router: appRouter,
+    createContext() {
+      const auth = getAuth(req);
+      return createContextInner({
+        req,
+        auth,
+      });
+    },
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+  });
+}
+
+export { handler as GET, handler as POST };
+
+export const runtime = "nodejs"
